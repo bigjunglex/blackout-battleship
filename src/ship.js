175731c32d496f10652fc65fe120d6a87a58a508
bigjunglex <gearl00taz@gmail.com>
2024-09-18 00:06:37 +0300

    ship implemented

--- src/ship.js
+++ src/ship.js
@@ -1,8 +1,13 @@
 class Ship {
     constructor(length){
         this.length = length;
-        this.hits = length;
+        this.hits = 0;
         this.status = true
+    }
+
+    hit(){
+        if (this.hits < this.length) ++this.hits
+        if (this.hits === this.length) this.status = false
     }
 }
 
--- test/ship.test.js
+++ test/ship.test.js
@@ -8,7 +8,18 @@ describe('Ship unit test', () => {
 
     test('ship creaction', () => {
         expect(ship.length).toBe(4)
-        expect(ship.hits).toBe(4)
+        expect(ship.hits).toBe(0)
         expect(ship.status).toBe(true)
+    })
+
+    test('ship hits and status change', () => {
+        ship.hit()
+        ship.hit()
+        expect(ship.hits).toBe(2)
+        expect(ship.status).toBe(true)
+        ship.hit()
+        ship.hit()
+        expect(ship.hits).toBe(4)
+        expect(ship.status).toBe(false)
     })
 })
