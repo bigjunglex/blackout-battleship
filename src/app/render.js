94927664290f1b1f8cb1b8d76a9b17ee997488a4
bigjunglex <gearl00taz@gmail.com>
2024-10-02 16:04:25 +0300

    checking DONT FORGET TO SQUASH

--- src/app/render.js
+++ src/app/render.js
@@ -1,7 +1,8 @@
 const renderBoard = (board, target) =>  {
     for (const i in board) {
         const cell = document.createElement('div')
-        board[i] === 0 ? cell.textContent = '' : cell.textContent = 'S'
+        cell.classList.add('cell')
+        board[i] === 0 ? cell.textContent = '1' : cell.textContent = 'S'
         target.appendChild(cell)
     }
 }
